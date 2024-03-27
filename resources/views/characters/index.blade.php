fe0bc572b1dc1c943f63b492d6405194e4900160
GiPisano <pisanogiacomo9@gmail.com>
2024-03-27 17:37:55 +0100

    define connection character index, charcater show, character create

--- resources/views/characters/index.blade.php
+++ resources/views/characters/index.blade.php
@@ -3,38 +3,41 @@
 @section('title', 'Characters')
 
 @section('main-content')
-<section>
-  <div class="container py-4">
-    <h1>Laravel with Bootstrap + Vite</h1>
-    <table class="table">
-      <thead>
-        <tr>
+    <section>
+        <div class="container py-4">
+            <h1>Characters list</h1>
+            <a href="{{ route('characters.create') }}" class="btn btn-primary">Create your character</a>
+            <table class="table">
+                <thead>
+                    <tr>
 
-          <th scope="col">name</th>
-          <th scope="col">attack</th>
-          <th scope="col">defence</th>
-          <th scope="col">speed</th>
-          <th scope="col">life</th>
-        </tr>
-      </thead>
-      <tbody>
-        @forelse ($characters as $character)
-        <tr>
-          <td>{{ $character->name }}</td>
-          <td>{{ $character->attack }}</td>
-          <td>{{ $character->defence }}</td>
-          <td>{{ $character->speed }}</td>
-          <td>{{ $character->life }}</td>
-        </tr>
-        @empty
-        <tr>
-          <td colspan="5">Nessun personaggio trovato</td>
-        </tr>
-        @endforelse
-      </tbody>
-    </table>
+                        <th scope="col">name</th>
+                        <th scope="col">attack</th>
+                        <th scope="col">defence</th>
+                        <th scope="col">speed</th>
+                        <th scope="col">life</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @forelse ($characters as $character)
+                        <tr>
+                            <td>
+                                <a href="{{ route('characters.show', $character) }}">{{ $character->name }}</a>
+                            </td>
+                            <td>{{ $character->attack }}</td>
+                            <td>{{ $character->defence }}</td>
+                            <td>{{ $character->speed }}</td>
+                            <td>{{ $character->life }}</td>
+                        </tr>
+                    @empty
+                        <tr>
+                            <td colspan="5">Nessun personaggio trovato</td>
+                        </tr>
+                    @endforelse
+                </tbody>
+            </table>
 
-  </div>
+        </div>
 
-</section>
+    </section>
 @endsection
