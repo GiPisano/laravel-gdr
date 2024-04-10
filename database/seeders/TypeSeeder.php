fcf531fd9d38c8c563c47167b9f067715e164c78
FrancescoCesari97 <freedus97@gmail.com>
2024-04-10 16:32:18 +0200

    seeder tabella type

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Type;
+use Illuminate\Support\Str;
 
 class TypeSeeder extends Seeder
 {
@@ -14,5 +16,24 @@ class TypeSeeder extends Seeder
      */
     public function run()
     {
+        $file = fopen(__DIR__ . '/../csv/types.csv', 'r');
+
+        $is_first_line = true;
+
+        while (!feof($file)) {
+            $data = fgetcsv($file);
+
+            if ($data) {
+                if (!$is_first_line) {
+                    $type = new Type();
+                    $type->name = $data[0];
+                    $type->slug = Str::slug($type->name, '-');
+                    $type->image = $data[1];
+                    $type->decription = $data[2];
+                    $type->save();
+                }
+            }
+            $is_first_line = false;
+        }
     }
 }
