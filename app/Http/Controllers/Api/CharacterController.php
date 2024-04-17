365fbdc9601942cbc5797c2b36b1e7dba5b34725
yusef02 <youssef.bouziane2402@gmail.com>
2024-04-17 18:50:47 +0200

    add id to api characters collection

--- app/Http/Controllers/Api/CharacterController.php
+++ app/Http/Controllers/Api/CharacterController.php
@@ -15,7 +15,7 @@ class CharacterController extends Controller
      */
     public function index()
     {
-        $characters = Character::select('name', 'description', 'strength', 'defence', 'speed', 'intelligence', 'life', 'type_id')->with('type')->get();
+        $characters = Character::select('id', 'name', 'description', 'strength', 'defence', 'speed', 'intelligence', 'life', 'type_id')->with('type')->get();
         return response()->json($characters);
     }
 
@@ -38,9 +38,9 @@ class CharacterController extends Controller
      */
     public function show($id)
     {
-        $user_character = Character::select('name', 'description', 'strength', 'defence', 'speed', 'intelligence', 'life', 'type_id')->with('type')->where('id', $id)->first();
+        $user_character = Character::select('id', 'name', 'description', 'strength', 'defence', 'speed', 'intelligence', 'life', 'type_id')->with('type')->where('id', $id)->first();
         $rand_num = rand(1, Character::count());
-        $cpu_character = Character::select('name', 'description', 'strength', 'defence', 'speed', 'intelligence', 'life', 'type_id')->with('type')->where('id', $rand_num)->first();
+        $cpu_character = Character::select('id', 'name', 'description', 'strength', 'defence', 'speed', 'intelligence', 'life', 'type_id')->with('type')->where('id', $rand_num)->first();
         return response()->json(['user_character' => $user_character, 'cpu_character' => $cpu_character]);
     }
 
